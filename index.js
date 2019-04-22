802668fbbeb0a7535f1479c7f83a082438c4ce98
Rex Osariemen <rexosariemen@yahoo.com>
2019-04-22 23:32:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2) {
+  var result = [];
+  
+  for (var i = 0; i < arr1.length; i++) {
+    var musicians = arr1[i];
+    var instruments = arr2[i];
+    var str = musicians + ' plays ' + instruments;
+    result.push(str);
+  }
+  
+  return result;
+}
+
+
+function johnLennonFacts(arr) {
+  var newArr = [];
+  
+  var i = 0;
+  while (i < arr.length) {
+    var fact = arr[i];
+    var exclaimedFact = fact + '!!!';
+    newArr.push(exclaimedFact);
+    i++;
+  }
+  
+  return newArr;
+}
+
+
+function iLoveTheBeatles(num) {
+  var newArr = [];
+  var i = num;
+  
+  do {
+    var str = 'I love the Beatles!';
+    newArr.push(str);
+    i = num + 1;
+  } while (i < 15);
+  
+  return newArr;
+}
